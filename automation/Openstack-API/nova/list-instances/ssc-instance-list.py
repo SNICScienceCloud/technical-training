336da1c4d1e8c8ebee474127d0b5561c6f6c932f
Salman Toor <salman.toor@gmail.com>
2017-09-10 23:24:48 +0200

    Update ssc-instance-list.py

--- automation/Openstack-API/nova/list-instances/ssc-instance-list.py
+++ automation/Openstack-API/nova/list-instances/ssc-instance-list.py
@@ -9,25 +9,17 @@ loader = loading.get_plugin_loader('password')
 from os import environ as env
 import keystoneclient.v3.client as ksclient
 
-keystone = ksclient.Client(auth_url=env['OS_AUTH_URL'],
-                           username=env['OS_USERNAME'],
-                           password=env['OS_PASSWORD'],
-                           project_name=env['OS_PROJECT_NAME'],
-                           project_domain_name=env['OS_USER_DOMAIN_NAME'],
-                           project_id=env['OS_PROJECT_ID'],
-                           version=env['OS_IDENTITY_API_VERSION'],
-                           user_domain_name=env['OS_USER_DOMAIN_NAME'],
-                           region_name=env['OS_REGION_NAME'])
-
-endpoints = keystone.service_catalog.get_endpoints()
-
-for endpoint in endpoints:
-    for edp in endpoints[endpoint]:
-        if edp['interface'] == 'public':
-            print 'service: ', endpoint, ', region: ', edp['region'], ', public endpoint: ', edp['url']
-
+auth = loader.load_from_options(auth_url=env['OS_AUTH_URL'],
+                                username=env['OS_USERNAME'],
+                                password=env['OS_PASSWORD'],
+                                project_name=env['OS_PROJECT_NAME'],
+                                project_domain_name=env['OS_USER_DOMAIN_NAME'],
+                                project_id=env['OS_PROJECT_ID'],
+                                user_domain_name=env['OS_USER_DOMAIN_NAME'])            
 sess = session.Session(auth=auth)
 nova = client.Client('2.1', session=sess)
+
+print '\n Server List:\n'
 
 for server in nova.servers.list():
     print server.id, server.name
