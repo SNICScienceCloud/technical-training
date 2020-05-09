fd36eb76869d16d0f89d849293076af1701b35c2
sztoor <salman.toor@gmail.com>
2020-05-09 11:43:13 +0000

    Added more sections to the Ansible code

--- model-serving/openstack-client/single_node_with_docker_ansible_client/start_instances.py
+++ model-serving/openstack-client/single_node_with_docker_ansible_client/start_instances.py
@@ -29,7 +29,7 @@ auth = loader.load_from_options(auth_url=env['OS_AUTH_URL'],
 
 sess = session.Session(auth=auth)
 nova = client.Client('2.1', session=sess)
-print "user authorization completed."
+print ("user authorization completed.")
 
 image = nova.glance.find_image(image_name)
 
@@ -57,18 +57,18 @@ else:
 
 secgroups = ['default']
 
-print "Creating instances ... "
+print ("Creating instances ... ")
 instance_prod = nova.servers.create(name="prod_server_with_docker_"+str(identifier), image=image, flavor=flavor, key_name='sztoor',userdata=userdata_prod, nics=nics,security_groups=secgroups)
 instance_dev = nova.servers.create(name="dev_server_"+str(identifier), image=image, flavor=flavor, key_name='sztoor',userdata=userdata_dev, nics=nics,security_groups=secgroups)
 inst_status_prod = instance_prod.status
 inst_status_dev = instance_dev.status
 
-print "waiting for 10 seconds.. "
+print ("waiting for 10 seconds.. ")
 time.sleep(10)
 
 while inst_status_prod == 'BUILD' or inst_status_dev == 'BUILD':
-    print "Instance: "+instance_prod.name+" is in "+inst_status_prod+" state, sleeping for 5 seconds more..."
-    print "Instance: "+instance_dev.name+" is in "+inst_status_dev+" state, sleeping for 5 seconds more..."
+    print ("Instance: "+instance_prod.name+" is in "+inst_status_prod+" state, sleeping for 5 seconds more...")
+    print ("Instance: "+instance_dev.name+" is in "+inst_status_dev+" state, sleeping for 5 seconds more...")
     time.sleep(5)
     instance_prod = nova.servers.get(instance_prod.id)
     inst_status_prod = instance_prod.status
@@ -91,5 +91,5 @@ for network in instance_dev.networks[private_net]:
 if ip_address_dev is None:
     raise RuntimeError('No IP address assigned!')
 
-print "Instance: "+ instance_prod.name +" is in " + inst_status_prod + " state" + " ip address: "+ ip_address_prod
-print "Instance: "+ instance_dev.name +" is in " + inst_status_dev + " state" + " ip address: "+ ip_address_dev
+print ("Instance: "+ instance_prod.name +" is in " + inst_status_prod + " state" + " ip address: "+ ip_address_prod)
+print ("Instance: "+ instance_dev.name +" is in " + inst_status_dev + " state" + " ip address: "+ ip_address_dev)
